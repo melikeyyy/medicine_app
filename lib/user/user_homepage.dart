729fba60a2ec94c0768b3acffeb0b61431564a73
Melike Yıldırım <melikeyildirim718@gmail.com>
2022-01-09 17:41:19 +0300

    firebase bağlantısı

---
+++ lib/user/user_homepage.dart
@@ -0,0 +1,18 @@
+import 'package:telemedicine_application/service/auth_service.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  AuthService _authService = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+
+
+  }
+}
